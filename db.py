58fd1724eacdccc2ff332eb5fce4c851f1035afd
Omar Hassan <you@example.com>
2023-09-01 20:01:48 +0300

    upload gadget db

--- db.py
+++ db.py
@@ -40,3 +40,42 @@ def get_all_users(connection):
 	query = 'SELECT * FROM users'
 	cursor.execute(query)
 	return cursor.fetchall()
+
+
+
+
+def init_restaurant(connection):
+    cursor = connection.cursor()
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS restaurants (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL UNIQUE,
+            description TEXT,
+            image_url TEXT,
+            FOREIGN KEY (user_id) REFERENCES users (id)
+        )
+    ''')
+
+    connection.commit()
+    
+    
+
+def add_restaurant(connection, title, description, image_url=None):
+    cursor = connection.cursor()
+    query = '''INSERT INTO restaurants ( title, description, image_url) VALUES (?, ?, ?)'''
+    cursor.execute(query, ( title, description, image_url))
+    connection.commit()
+
+def get_restaurant(connection, restaurant_id):
+    cursor = connection.cursor()
+    query = '''SELECT * FROM restaurants WHERE id = ?'''
+    cursor.execute(query, (restaurant_id,))
+    return cursor.fetchone()
+
+
+def get_all_restaurants(connection):
+    cursor = connection.cursor()
+    query = '''SELECT * FROM restaurants'''
+    cursor.execute(query)
+    return cursor.fetchall()
