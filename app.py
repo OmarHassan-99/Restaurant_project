8bf2b6ddb83bc403b8f69da36365e4aafc73a853
Omar Hassan <you@example.com>
2023-09-01 22:32:06 +0300

    fixed upload page

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import db
 import utils
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+import validators
 
 app = Flask(__name__)
 connection = db.connect_to_database()
@@ -11,12 +12,15 @@ limiter = Limiter(app=app, key_func=get_remote_address, default_limits=["10 per 
 
 
 
-@app.route('/')
+@app.route('/index')
 def index():
             return render_template("index.html")
+        
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
+@limiter.limit("5 per minute") 
 def login():
     if request.method == 'POST':
         email = request.form['emailorUsername']
@@ -40,6 +44,7 @@ def login():
     return render_template('login.html')
 
 @app.route('/signup', methods=['GET', 'POST'])
+@limiter.limit("5 per minute") 
 def sign_up():
     if request.method == 'POST':
         first_name=request.form['first_name']
@@ -58,6 +63,51 @@ def sign_up():
             return redirect(url_for('login'))
 
     return render_template('signup.html')
+
+
+
+@app.route('/upload-restaurant', methods=['GET', 'POST'])
+@limiter.limit("10 per minute") 
+def uploadrestaurant():
+
+    if request.method == 'POST':
+        restaurantImage = request.files['image']
+        if not restaurantImage or restaurantImage.filename == '':
+            flash("Image Is Required", "danger")
+            return render_template("upload-restaurant.html")
+
+        if not validators.allowed_file(restaurantImage.filename) or not validators.allowed_file_size(restaurantImage):
+            flash("Invalid File is Uploaded", "danger")
+            return render_template("upload-restaurant.html")
+
+        title = request.form['title']
+        description = request.form['description']
+    
+        image_url = f"uploads/{restaurantImage.filename}"
+        restaurantImage.save("assets/images/" + image_url)
+        user_id = session['user_id']
+        db.add_restaurant(connection, user_id, title, description,image_url)
+        return redirect(url_for('index'))
+    return render_template('upload-restaurant.html')
+
+
+app.route('/restaurant/<restaurant_id>')
+def getrestaurant(restaurant_id):
+	restaurant = db.get_restaurant(connection, restaurant_id)
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @app.route('/logout')
 def logout():
